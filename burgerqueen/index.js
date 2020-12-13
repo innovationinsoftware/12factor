2cc2ae84579f7e4f09b22db02bcc1de858f5a0ee
Bob Reselman <reselbob@gmail.com>
2020-12-12 21:01:55 -0800

    updated process-6 code

--- burgerqueen/index.js
+++ burgerqueen/index.js
@@ -1,23 +1,16 @@
 const http = require('http');
 const port = process.env.APP_PORT || 3000;
-const request = require('request-promise');
-
-const service = 'burgerqueen';
-
-const initTracer = require('./tracer').initTracer;
-const { Tags, FORMAT_HTTP_HEADERS } = require('opentracing');
-const tracer = initTracer(service);
 
 const restaurant = 'Burger Queen';
 
 const foods = ['burger', 'fries', 'whooper', 'onion rings']
 
-const sample = (items) => { return items[Math.floor(Math.random() * items.length)]; };
+const sample = (items) => {return items[Math.floor(Math.random()*items.length)];};
 
 const shutdown = (signal) => {
-    if (!signal) {
+    if(!signal){
         console.log(`${restaurant} API Server shutting down at ${new Date()}`);
-    } else {
+    }else{
         console.log(`Signal ${signal} : ${restaurant} API Server shutting down at ${new Date()}`);
     }
     server.close(function () {
@@ -25,74 +18,13 @@ const shutdown = (signal) => {
     })
 };
 
-const handleRequest = async (request, response) => {
-    parentSpanContext = tracer.extract(FORMAT_HTTP_HEADERS, request.headers)
-    const span = tracer.startSpan('restaurant_service_request', {
-        childOf: parentSpanContext,
-        tags: { [Tags.SPAN_KIND]: Tags.SPAN_KIND_RPC_SERVER,  'business_group': 'restaurant'}
-    });
-
-    span.log({
-        'event': 'request_headers',
-        'value': request.headers
-    });
-
-    const order = sample(foods);
-    const purchase = { service, item: order, amount: 0 };
-    const data = await callPaymentService(purchase, span);
-
-    span.setTag('identified_order', order);
-    span.log({
-        'event': 'burgerqueen_service_request',
-        'value': data
-    });
-
-    //let data = {restaurant, order, transactionId: 1, status:'OK'}
-    const obj = { restaurant, order };
-    const str = JSON.stringify(obj);
-
-    span.setTag(Tags.HTTP_STATUS_CODE, 200);
-    span.setTag('burgerqueen_call_result', str);
-
+const server = http.createServer((request, response) => {
+    const order = sample(foods) ;
+    const str = JSON.stringify({restaurant, order});
     response.setHeader("Content-Type", "application/json");
     response.writeHead(200);
     response.end(str);
-
-    span.finish()
-}
-
-const callPaymentService = async (payload, root_span) => {
-    const span = tracer.startSpan('call_service', { childOf: root_span.context() });
-    span.setTag(Tags.SPAN_KIND, Tags.SPAN_KIND_RPC_CLIENT);
-    const headers = {};
-    tracer.inject(span.context(), FORMAT_HTTP_HEADERS, headers);
-    span.log({
-        'event':'call_service_header',
-        'value':JSON.stringify( headers)
-    });
-
-    const method = 'GET';
-    const service = 'payments';
-    const url = `http://${service}:${port}`;
-    return request({ url, method, headers })
-        .then(data => {
-            span.setTag(Tags.HTTP_STATUS_CODE, 200)
-            span.setTag('service_call_result', data)
-            span.finish();
-            return data;
-        }, e => {
-            span.setTag(Tags.ERROR, true)
-            span.log({
-                'event': 'error',
-                'error.object': e
-            });
-            span.finish();
-        });
-};
-
-const server = http.createServer(handleRequest);
-
-server.listen(port, (err) => {
+}).listen(port, (err) => {
     console.log(`${restaurant} API Server is started on ${port}  at ${new Date()} with pid ${process.pid}`);
 });
 
@@ -105,4 +37,4 @@ process.on('SIGINT', function () {
 });
 
 
-module.exports = { server, shutdown };
+module.exports = {server,shutdown};
--- customer/index.js
+++ customer/index.js
@@ -1,72 +1,42 @@
-
 const http = require('http');
-const request = require('request-promise');
 const port = process.env.APP_PORT || 3000;
-
-const initTracer = require('./tracer').initTracer;
-const { Tags, FORMAT_HTTP_HEADERS } = require('opentracing');
-const tracer = initTracer('customer');
 
 const customer = 'Friendly Shopper';
 
 const DEFAULT_SERVICES = ['burgerqueen', 'hobos', 'iowafried'];
 let services = DEFAULT_SERVICES;
 
-const sample = (items) => { return items[Math.floor(Math.random() * items.length)]; };
+const sample = (items) => {return items[Math.floor(Math.random()*items.length)];};
 
-const handleRequest = async (request, response) => {
-    parentSpanContext = tracer.extract(FORMAT_HTTP_HEADERS, request.headers)
-    const span = tracer.startSpan('customer_service_get', {
-        childOf: parentSpanContext,
-        tags: { [Tags.SPAN_KIND]: Tags.SPAN_KIND_RPC_SERVER }
-    });
-
-    span.log({
-        'event': 'request_headers',
-        'value': request.headers
-    });
+const handleRequest = (request, response)  => {
 
     const service = sample(services);
-    span.setTag('indentified_service', service);
-    span.log({
-        'event': 'customer_service_get',
-        'value': service
-    });
 
-    const data = await callService(service, span);
-    const obj = JSON.parse(data);
-    obj.customer = customer;
-    const str = JSON.stringify(obj);
-    span.setTag(Tags.HTTP_STATUS_CODE, 200)
-    span.setTag('service_call_result', str)
-    response.setHeader("Content-Type", "application/json");
-    response.writeHead(200);
-    response.end(str);
+    http.get(`http://${service}:${port}`, (resp) => {
+        let data = '';
 
-    span.finish();
-};
-
-const callService = async (service, root_span) => {
-    const method = 'GET';
-    const headers = {};
-    const url = `http://${service}:${port}`;
-    const span = tracer.startSpan('call_service', { childOf: root_span.context() });
-    tracer.inject(span, FORMAT_HTTP_HEADERS, headers);
-    return request({ url, method, headers })
-        .then(data => {
-            span.setTag(Tags.HTTP_STATUS_CODE, 200)
-            span.setTag('service_call_result', data)
-            span.finish();
-            return data;
-        }, e => {
-            span.setTag(Tags.ERROR, true)
-            span.log({
-                'event': 'error',
-                'error.object': e
-            });
-            span.finish();
+        // A chunk of data has been recieved.
+        resp.on('data', (chunk) => {
+            data += chunk;
         });
 
+        // The whole response has been received. Print out the result.
+        resp.on('end', () => {
+            const obj = JSON.parse(data);
+            obj.customer = customer;
+            const str = JSON.stringify(obj);
+            response.setHeader("Content-Type", "application/json");
+            response.writeHead(200);
+            response.end(str);
+        });
+
+    }).on("error", (err) => {
+        console.log("Error: " + err.message);
+        const str = JSON.stringify(err);
+        response.setHeader("Content-Type", "application/json");
+        response.writeHead(500);
+        response.end(str);
+    });
 };
 
 const server = http.createServer(handleRequest);
@@ -77,24 +47,11 @@ server.listen(port, () => {
 
 
 const shutdown = (signal) => {
-    const span = tracer.startSpan('customer_service-shutdown');
-    if (!signal) {
-        const str = `${customer} API Server shutting down at ${new Date()}`
-        span.log({
-            'event': 'customer_service_shutdown',
-            'value': str
-        });
-        console.log(str);
-
-    } else {
-        const str = `Signal ${signal} : ${customer} API Server shutting down at ${new Date()}`
-        span.log({
-            'event': 'customer_service_shutdown',
-            'value': str
-        });
-        console.log(str);
+    if(!signal){
+        console.log(`${customer} API Server shutting down at ${new Date()}`);
+    }else{
+        console.log(`Signal ${signal} : ${customer} API Server shutting down at ${new Date()}`);
     }
-    span.finish();
     server.close(function () {
         process.exit(0);
     })
@@ -107,4 +64,4 @@ process.on('SIGINT', function () {
     shutdown('SIGINT');
 });
 
-module.exports = { server, shutdown };
+module.exports = {server,shutdown};
