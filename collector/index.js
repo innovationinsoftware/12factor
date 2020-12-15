4a2459b8b688414a503fab15cd4bdabcb7df8a7d
Bob Reselman <reselbob@gmail.com>
2020-12-15 13:42:00 -0800

    fixing -8 code

--- collector/index.js
+++ collector/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const uuidv4 = require('uuid/v4');
 
-//const {write, read} = require('./datastore')
+const {write, read} = require('./datastore')
 
-if(!process.env.COLLECTOR_PORT)throw Error('The required environment variable, COLLECTOR_PORT is not defined in collector.')
+if(!process.env.COLLECTOR_PORT)throw Error('The required environment variable, COLLECTOR_PORT is not defined.')
 const port = process.env.COLLECTOR_PORT;
 
 const app = express();
@@ -19,22 +19,25 @@ app.post("/", async (request, response) => {
     // add our unix time as a "created" property and add it to our request.body
     const newData = Object.assign({"created": unixTimeCreated}, request.body);
     newData.correlationId = correlationId;
-    //const rslt = await write(newData);
+    const rslt = await write(newData);
 
     response.writeHead(200);
-    response.end(JSON.stringify({datawrite:newData}));
-    console.log({datawrite:newData});
+    response.end(JSON.stringify(rslt));
+    console.log(rslt);
 })
 
 //GET
 app.get("/", async (request, response) => {
     const correlationId = request.header('x-correlation-id') || uuidv4();
-    //const key = request.query.key
-    //const rslt = await read(key);
+    const key = request.query.key
+    const rslt = await read(key);
     response.writeHead(200);
-    response.end(JSON.stringify({dataread:correlationId}));
-    console.log({dataread:correlationId});
+    response.end(JSON.stringify(rslt));
+    console.log(rslt);
 })
+
+
+
 
 const server = app.listen(port, () => {
     console.log(`Collector Server started on port ${port} at ${new Date()}`)
